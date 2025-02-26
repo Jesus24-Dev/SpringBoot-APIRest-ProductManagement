f660c2b14a855037d3dbcdacd6c33e952eb621a3
Jesus24-Dev <siritjesus24@gmail.com>
2025-02-26 10:34:41 -0400

    Added validation on controller

--- src/main/java/com/productmanagement/api_products/controllers/ProductController.java
+++ src/main/java/com/productmanagement/api_products/controllers/ProductController.java
@@ -3,9 +3,12 @@ package com.productmanagement.api_products.controllers;
 
 import com.productmanagement.api_products.models.Product;
 import com.productmanagement.api_products.services.ProductService;
+import jakarta.validation.Valid;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -33,13 +36,15 @@ public class ProductController {
     }
     
     @PostMapping
-    public Product createProduct(@RequestBody Product product){
-        return productService.createProduct(product);
+    public ResponseEntity<String> createProduct(@Valid @RequestBody Product product){
+        productService.createProduct(product);
+        return ResponseEntity.ok("User created");
     }
     
     @PutMapping("/{id}")
-    public Product updateProduct(@PathVariable Long id, @RequestBody Product productDetails){
-        return productService.updateProduct(id, productDetails);
+    public ResponseEntity<String> updateProduct(@Valid @PathVariable Long id, @RequestBody Product productDetails){
+        productService.updateProduct(id, productDetails);
+        return ResponseEntity.ok("User updated");
     }
     
     @DeleteMapping("/{id}")
