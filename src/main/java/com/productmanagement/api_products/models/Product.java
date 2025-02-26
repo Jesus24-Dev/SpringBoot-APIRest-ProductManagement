7c14ca3a114b6e7b352d0a72d332d01e4f7d5ef4
Jesus24-Dev <siritjesus24@gmail.com>
2025-02-26 10:35:07 -0400

    Add SQL validations on Product model

--- src/main/java/com/productmanagement/api_products/models/Product.java
+++ src/main/java/com/productmanagement/api_products/models/Product.java
@@ -2,15 +2,22 @@
 package com.productmanagement.api_products.models;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 @Entity
 public class Product {
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     
     @Column
+    @NotBlank(message = "Name can't be empty")
+    @Size(max = 50, message = "Name can't be longer than 50 characters")
     private String name;
+    
+    @Min(value = 0, message = "Price must be at least 0")
     private Double price;
 
     public Product() {
