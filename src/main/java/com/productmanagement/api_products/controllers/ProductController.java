8e5e2f5ff5df457a6dc59a3ca0d51997f99756b4
Jesus24-Dev <siritjesus24@gmail.com>
2025-02-26 11:17:54 -0400

    Add validators and exceptions handlers in controllers

--- src/main/java/com/productmanagement/api_products/controllers/ProductController.java
+++ src/main/java/com/productmanagement/api_products/controllers/ProductController.java
@@ -1,13 +1,12 @@
 
 package com.productmanagement.api_products.controllers;
 
+import com.productmanagement.api_products.exceptions.ProductNotFoundException;
 import com.productmanagement.api_products.models.Product;
 import com.productmanagement.api_products.services.ProductService;
 import jakarta.validation.Valid;
 import java.util.List;
-import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -27,28 +26,39 @@ public class ProductController {
     
     @GetMapping
     public List<Product> getAllProducts(){
-        return productService.getAllProducts();
+        try {
+            return productService.getAllProducts();
+        } catch(Exception e) {
+            throw new RuntimeException("Error fetching products", e);
+        }       
     }
     
     @GetMapping("/{id}")
-    public Optional<Product> getProductById(@PathVariable Long id){
-        return productService.getProductById(id);
+    public ResponseEntity<Product> getProductById(@PathVariable Long id) {
+        Product product = productService.getProductById(id)
+                .orElseThrow(() -> new ProductNotFoundException("Product [" + id + "] not found"));
+        return ResponseEntity.ok(product);
     }
-    
+
     @PostMapping
     public ResponseEntity<String> createProduct(@Valid @RequestBody Product product){
         productService.createProduct(product);
-        return ResponseEntity.ok("User created");
+        return ResponseEntity.ok("User created");           
     }
     
     @PutMapping("/{id}")
     public ResponseEntity<String> updateProduct(@Valid @PathVariable Long id, @RequestBody Product productDetails){
-        productService.updateProduct(id, productDetails);
-        return ResponseEntity.ok("User updated");
+        Product product = productService.getProductById(id)
+            .orElseThrow(() -> new ProductNotFoundException("Product [" + id + "] not found"));
+        productService.updateProduct(product.getId(), productDetails);
+        return ResponseEntity.ok("User updated");             
     }
     
     @DeleteMapping("/{id}")
-    public void deleteProduct(@PathVariable Long id){
-        productService.deleteProduct(id);
+    public ResponseEntity<String> deleteProduct(@PathVariable Long id){
+        Product product = productService.getProductById(id)
+            .orElseThrow(() -> new ProductNotFoundException("Product [" + id + "] not found"));
+        productService.deleteProduct(product.getId());
+        return ResponseEntity.ok("Product deleted");
     }
 }
