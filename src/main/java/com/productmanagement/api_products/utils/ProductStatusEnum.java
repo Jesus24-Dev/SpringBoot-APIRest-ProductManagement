961b7c0fadbdd59357820abe498810308009ebe1
Jesus24-Dev <siritjesus24@gmail.com>
2025-02-27 23:34:43 -0400

    Enums for the classes

---
+++ src/main/java/com/productmanagement/api_products/utils/ProductStatusEnum.java
@@ -0,0 +1,11 @@
+
+package com.productmanagement.api_products.utils;
+
+public class ProductStatusEnum {
+    public enum ProductStatus {
+        PENDING,
+        IN_PROGRESS,
+        SENT,
+        CANCELED
+    }
+}
