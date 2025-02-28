db616cea3f2206a300c15ee75528e4e140443e53
Jesus24-Dev <siritjesus24@gmail.com>
2025-02-27 23:33:26 -0400

    Customer entity class created. Added validators

---
+++ src/main/java/com/productmanagement/api_products/models/Customer.java
@@ -0,0 +1,61 @@
+
+package com.productmanagement.api_products.models;
+
+import com.productmanagement.api_products.utils.CustomerRoleEnum;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Entity
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @NotBlank(message = "Name can't be empty")
+    private String name;
+
+    @NotBlank(message = "Password can't be empty")
+    private String password;
+
+    private CustomerRoleEnum.Role role;
+
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
+
+    public Customer() {
+        this.role = CustomerRoleEnum.Role.CUSTOMER; 
+    }
+
+    public Customer(String name, String password) {
+        this.name = name;
+        this.password = passwordEncoder.encode(password); 
+        this.role = CustomerRoleEnum.Role.CUSTOMER;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = passwordEncoder.encode(password); 
+    }
+
+    public CustomerRoleEnum.Role getRole() {
+        return role;
+    }
+
+    public void setRole(CustomerRoleEnum.Role role) {
+        this.role = role;
+    }
+}
