aff31697e11f66d2318d8dff91c7003c8691229e
Jesus24-Dev <siritjesus24@gmail.com>
2025-02-28 00:18:40 -0400

    Change table name to customer-orders

--- src/main/java/com/productmanagement/api_products/models/Order.java
+++ src/main/java/com/productmanagement/api_products/models/Order.java
@@ -14,6 +14,7 @@ import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 @Entity
+@Table(name = "customer_orders") 
 public class Order {
 
     @Id
