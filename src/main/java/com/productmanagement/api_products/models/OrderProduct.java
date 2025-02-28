fa21fff8183b78e8e6b7d2022d10c287e0c1f6c3
Jesus24-Dev <siritjesus24@gmail.com>
2025-02-27 23:58:33 -0400

    ProductOrder entity created. Added validators

---
+++ src/main/java/com/productmanagement/api_products/models/OrderProduct.java
@@ -0,0 +1,93 @@
+
+package com.productmanagement.api_products.models;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+@Entity
+public class OrderProduct {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+    
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    @NotNull(message = "Product id can't be empty")
+    private Product product;
+    
+    @Min(value = 0, message = "Count must be at least 0")
+    private int quantity;
+    
+    @Min(value = 0, message = "Total price must be at least 0")
+    private Double totalPrice;
+    
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+    
+    public OrderProduct() {
+    }
+
+    public OrderProduct(Order order, Product product, int quantity, Double totalPrice) {
+        this.order = order;
+        this.product = product;
+        this.quantity = quantity;
+        this.totalPrice = totalPrice;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public Double getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(Double totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+    
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+}
+
