64bf844037b622637bd5dd0beabb4a9274430fb7
Jesus24-Dev <siritjesus24@gmail.com>
2025-02-28 21:30:31 -0400

    Created Order Product Service with CRUD Functions

---
+++ src/main/java/com/productmanagement/api_products/services/OrderProductService.java
@@ -0,0 +1,43 @@
+
+package com.productmanagement.api_products.services;
+
+import com.productmanagement.api_products.exceptions.OrderProductNotFoundException;
+import com.productmanagement.api_products.models.OrderProduct;
+import com.productmanagement.api_products.repository.OrderProductRepository;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderProductService {
+    
+    @Autowired
+    private OrderProductRepository orderProductRepository;
+    
+    public List<OrderProduct> getAllOrdersProducts(){
+        return orderProductRepository.findAll();
+    }
+    public OrderProduct getOrderProductById(Long id){
+        return orderProductRepository.findById(id)
+                .orElseThrow(() -> new OrderProductNotFoundException("Order Products [" + id + "] not found"));
+    }
+    public void createOrderProduct(OrderProduct orderProduct){
+        orderProductRepository.save(orderProduct);
+    }
+    public void updateOrderProduct(Long id, OrderProduct orderProductDetails){
+        OrderProduct existingOrderProduct = orderProductRepository.findById(id)
+                .orElseThrow(() -> new OrderProductNotFoundException("Order Products [" + id + "] not found"));
+        
+        existingOrderProduct.setOrder(orderProductDetails.getOrder());
+        existingOrderProduct.setProduct(orderProductDetails.getProduct());
+        existingOrderProduct.setQuantity(orderProductDetails.getQuantity());
+        existingOrderProduct.setTotalPrice(orderProductDetails.getTotalPrice());
+    }
+    public void deleteOrderProduct(Long id){
+        OrderProduct existingOrderProduct = orderProductRepository.findById(id)
+                .orElseThrow(() -> new OrderProductNotFoundException("Order Products [" + id + "] not found"));
+        
+        orderProductRepository.delete(existingOrderProduct);
+    }
+    
+}
