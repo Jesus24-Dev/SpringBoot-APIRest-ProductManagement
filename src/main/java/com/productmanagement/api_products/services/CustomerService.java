bd93ba3aedcb5f304cfd221415138d2cf5d8e77f
Jesus24-Dev <siritjesus24@gmail.com>
2025-02-28 21:18:17 -0400

    Created Customer Service class

---
+++ src/main/java/com/productmanagement/api_products/services/CustomerService.java
@@ -0,0 +1,42 @@
+
+package com.productmanagement.api_products.services;
+
+import com.productmanagement.api_products.exceptions.CustomerNotFoundException;
+import com.productmanagement.api_products.models.Customer;
+import com.productmanagement.api_products.repository.CustomerRepository;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomerService {
+    
+    @Autowired
+    private CustomerRepository customerRepository;
+    
+    public List<Customer> getAllCustomer(){
+        return customerRepository.findAll();
+    }
+    public Customer getCustomerById(Long id){
+        return customerRepository.findById(id)
+                .orElseThrow(() -> new CustomerNotFoundException("Customer [" + id + "] not found"));
+    }
+    public void createCustomer(Customer customer){
+        customerRepository.save(customer);
+    }
+    public void updateCustomer(Long id, Customer customerDetails){
+        Customer existingCustomer = customerRepository.findById(id)
+                .orElseThrow(() -> new CustomerNotFoundException("Customer [" + id + "] not found"));
+        
+        existingCustomer.setName(customerDetails.getName());
+        existingCustomer.setName(customerDetails.getName());
+        
+        customerRepository.save(existingCustomer);
+    }
+    public void deleteCustomer(Long id){
+        Customer existingCustomer = customerRepository.findById(id)
+                .orElseThrow(() -> new CustomerNotFoundException("Customer [" + id + "] not found"));
+        
+        customerRepository.delete(existingCustomer);
+    }
+}
