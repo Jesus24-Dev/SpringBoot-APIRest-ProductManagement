9fb397a1d4f78700ae81fff300439abd029a7b3b
Jesus24-Dev <siritjesus24@gmail.com>
2025-03-04 14:26:29 -0400

    Return entitied used. Add transactional anotation

--- src/main/java/com/productmanagement/api_products/services/CategoryService.java
+++ src/main/java/com/productmanagement/api_products/services/CategoryService.java
@@ -7,39 +7,42 @@ import com.productmanagement.api_products.repository.CategoryRepository;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class CategoryService {
-    
+
     @Autowired
     private CategoryRepository categoryRepository;
-    
-    public List<Category> getAllCategories(){
+
+    public List<Category> getAllCategories() {
         return categoryRepository.findAll();
     }
-    
-    public Category getCategoryById(Long id){
+
+    public Category getCategoryById(Long id) {
         return categoryRepository.findById(id)
                 .orElseThrow(() -> new CategoryNotFoundException("Category [" + id + "] not found"));
     }
-    
-    public void createCategory(Category category){
-        categoryRepository.save(category);
-    } 
-    
-    public void updateCategory(Long id, Category categoryDetails){
-        Category existingCategory = categoryRepository.findById(id)
-                .orElseThrow(() -> new CategoryNotFoundException("Category [" + id + "] not found"));
-        
-        existingCategory.setName(categoryDetails.getName());
-        
-        categoryRepository.save(existingCategory);
+
+    @Transactional
+    public Category createCategory(Category category) {
+        return categoryRepository.save(category);
     }
-    
-    public void deleteCategory(Long id){
+
+    @Transactional
+    public Category updateCategory(Long id, Category categoryDetails) {
         Category existingCategory = categoryRepository.findById(id)
                 .orElseThrow(() -> new CategoryNotFoundException("Category [" + id + "] not found"));
-        
+
+        existingCategory.setName(categoryDetails.getName());
+        return categoryRepository.save(existingCategory);
+    }
+
+    @Transactional
+    public void deleteCategory(Long id) {
+        Category existingCategory = categoryRepository.findById(id)
+                .orElseThrow(() -> new CategoryNotFoundException("Category [" + id + "] not found"));
+
         categoryRepository.delete(existingCategory);
     }
 }
