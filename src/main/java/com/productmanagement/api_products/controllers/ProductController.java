4aabbf9ad04956017ad745e7fa6759b7803355b2
Jesus24-Dev <siritjesus24@gmail.com>
2025-03-04 14:48:35 -0400

    Add response entity in HTTP responses

--- src/main/java/com/productmanagement/api_products/controllers/ProductController.java
+++ src/main/java/com/productmanagement/api_products/controllers/ProductController.java
@@ -1,12 +1,12 @@
 
 package com.productmanagement.api_products.controllers;
 
-import com.productmanagement.api_products.exceptions.ProductNotFoundException;
 import com.productmanagement.api_products.models.Product;
 import com.productmanagement.api_products.services.ProductService;
 import jakarta.validation.Valid;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -25,8 +25,9 @@ public class ProductController {
     private ProductService productService;
     
     @GetMapping
-    public List<Product> getAllProducts() {
-        return productService.getAllProducts();
+    public ResponseEntity<List<Product>> getAllProducts() {
+        List<Product> products = productService.getAllProducts();
+        return ResponseEntity.ok(products);
     }
     
     @GetMapping("/{id}")
@@ -36,20 +37,20 @@ public class ProductController {
     }
 
     @PostMapping
-    public ResponseEntity<String> createProduct(@Valid @RequestBody Product product) {
-        productService.createProduct(product);
-        return ResponseEntity.ok("Product created");
+    public ResponseEntity<Product> createProduct(@Valid @RequestBody Product product) {
+        Product createdProduct = productService.createProduct(product);
+        return new ResponseEntity<>(createdProduct, HttpStatus.CREATED);
     }
     
     @PutMapping("/{id}")
-    public ResponseEntity<String> updateProduct(@Valid @PathVariable Long id, @RequestBody Product productDetails) {
-        productService.updateProduct(id, productDetails);
-        return ResponseEntity.ok("Product updated");
+    public ResponseEntity<Product> updateProduct(@Valid @PathVariable Long id, @RequestBody Product productDetails) {
+        Product updatedProduct = productService.updateProduct(id, productDetails);
+        return ResponseEntity.ok(updatedProduct);
     }
     
     @DeleteMapping("/{id}")
     public ResponseEntity<String> deleteProduct(@PathVariable Long id) {
         productService.deleteProduct(id);
-        return ResponseEntity.ok("Product deleted");
+        return ResponseEntity.noContent().build();
     }
 }
