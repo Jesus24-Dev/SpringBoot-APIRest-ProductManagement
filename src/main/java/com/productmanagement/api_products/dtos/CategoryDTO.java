6f08f43b860def9ad0fc35e99ead170cbc610944
Jesus24-Dev <siritjesus24@gmail.com>
2025-03-11 12:41:33 -0400

    created categoryDTO class

---
+++ src/main/java/com/productmanagement/api_products/dtos/CategoryDTO.java
@@ -0,0 +1,32 @@
+
+package com.productmanagement.api_products.dtos;
+
+import com.productmanagement.api_products.models.Category;
+
+public class CategoryDTO {
+    private Long id;
+    private String name;
+    
+    public CategoryDTO(Category category){
+        this.id = category.getId();
+        this.name = category.getName();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    
+}
