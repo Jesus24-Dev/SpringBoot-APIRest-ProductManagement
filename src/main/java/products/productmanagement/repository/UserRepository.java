1a62c339597ff7bb15efbffdc69618f28bfe5c37
Jesus24-Dev <siritjesus24@gmail.com>
2025-04-14 16:13:21 -0400

    Add findByUsername method to UserRepository interface

--- src/main/java/products/productmanagement/repository/UserRepository.java
+++ src/main/java/products/productmanagement/repository/UserRepository.java
@@ -1,9 +1,10 @@
 
 package products.productmanagement.repository;
 
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import products.productmanagement.models.User;
 
 public interface UserRepository extends JpaRepository<User, Long>{
-    
+    Optional<User> findByUsername(String username);
 }
